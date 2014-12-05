eb12a42fa26af1d91370c25da84114c4bbf0d53c
Erkan <etaylan13@gmail.com>
2014-12-05 13:53:20 +0200

    Added First Screen

---
+++ Process-Scheduling-Algorithms/Process-Scheduling-Algorithms/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows;
+
+namespace Process_Scheduling_Algorithms
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region COMBOBOX_ITEMS_ONSELECTED
+        //TODO
+
+        private void CbiFCFS_OnSelected(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                lblFCFS.Visibility = Visibility.Visible;
+                lblPNP.Visibility = Visibility.Hidden;
+                lblPP.Visibility = Visibility.Hidden;
+                lblRR.Visibility = Visibility.Hidden;
+                lblSJF.Visibility = Visibility.Hidden;
+                lblSRTF.Visibility = Visibility.Hidden;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void CbiSJF_OnSelected(object sender, RoutedEventArgs e)
+        {
+            lblFCFS.Visibility = Visibility.Hidden;
+            lblPNP.Visibility = Visibility.Hidden;
+            lblPP.Visibility = Visibility.Hidden;
+            lblRR.Visibility = Visibility.Hidden;
+            lblSJF.Visibility = Visibility.Visible;
+            lblSRTF.Visibility = Visibility.Hidden;
+        }
+
+        private void CbiSRTF_OnSelected(object sender, RoutedEventArgs e)
+        {
+            lblFCFS.Visibility = Visibility.Hidden;
+            lblPNP.Visibility = Visibility.Hidden;
+            lblPP.Visibility = Visibility.Hidden;
+            lblRR.Visibility = Visibility.Hidden;
+            lblSJF.Visibility = Visibility.Hidden;
+            lblSRTF.Visibility = Visibility.Visible;
+        }
+
+        private void CbiPP_OnSelected(object sender, RoutedEventArgs e)
+        {
+            lblFCFS.Visibility = Visibility.Hidden;
+            lblPNP.Visibility = Visibility.Hidden;
+            lblPP.Visibility = Visibility.Visible;
+            lblRR.Visibility = Visibility.Hidden;
+            lblSJF.Visibility = Visibility.Hidden;
+            lblSRTF.Visibility = Visibility.Hidden;
+        }
+
+        private void CbiPNP_OnSelected(object sender, RoutedEventArgs e)
+        {
+            lblFCFS.Visibility = Visibility.Hidden;
+            lblPNP.Visibility = Visibility.Visible;
+            lblPP.Visibility = Visibility.Hidden;
+            lblRR.Visibility = Visibility.Hidden;
+            lblSJF.Visibility = Visibility.Hidden;
+            lblSRTF.Visibility = Visibility.Hidden;
+        }
+
+        private void CbiRR_OnSelected(object sender, RoutedEventArgs e)
+        {
+            lblFCFS.Visibility = Visibility.Hidden;
+            lblPNP.Visibility = Visibility.Hidden;
+            lblPP.Visibility = Visibility.Hidden;
+            lblRR.Visibility = Visibility.Visible;
+            lblSJF.Visibility = Visibility.Hidden;
+            lblSRTF.Visibility = Visibility.Hidden;
+        } 
+        #endregion
+    }
+}
